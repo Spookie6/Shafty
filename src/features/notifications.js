3d693ed4f999ef4c7927ac202e01a52546e2a08c
Enrico <enricovdarend@gmail.com>
2024-04-12 17:01:16 +0200

    fixed mineshaft alert color

--- src/features/notifications.js
+++ src/features/notifications.js
@@ -3,7 +3,7 @@ import { registerWhen } from "../util/functions";
 
 registerWhen(
     register("chat", () => {
-        Client.showTitle("$bMINESHAFT", "", 1, 20, 1)
+        Client.showTitle("&bMINESHAFT", "", 1, 20, 1)
     }).setCriteria("WOW! You found a Glacite Mineshaft portal!"),
     () => Settings.shaftAlert
 )
